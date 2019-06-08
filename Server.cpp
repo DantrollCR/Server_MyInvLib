7459d1cf08ee022ddb301ff720008553f8da11ef
Daniel Acuña <daniel-2443@hotmail.com>
2019-06-08 07:16:30 -0600

    Server.

---
+++ Server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dantroll on 08/06/19.
+//
+
+#include "Server.h"
---
+++ Server.h
@@ -0,0 +1,15 @@
+//
+// Created by dantroll on 08/06/19.
+//
+
+#ifndef SERVER_MYINVLIB_SERVER_H
+#define SERVER_MYINVLIB_SERVER_H
+
+
+class Server {
+public:
+    void init();
+};
+
+
+#endif //SERVER_MYINVLIB_SERVER_H
